ad4ef4aa0c854b27e5c3198e99777463f1be4867
byeonyonghyun~ <qusdydgus0113@gmail.com>
2024-02-13 18:30:54 +0900

    240213

---
+++ 240213/script.js
@@ -0,0 +1,48 @@
+window.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector("button");
+  button.addEventListener("click", () => {
+    //   // const name = "변용현";
+    //   // window.write("제 이름은" + name + "입니다");
+    //   // // alert("dhsmfeh ghkdlxld!!");
+    //   // confirm("오늘도 화이팅");
+    //   // window.prompt("ekdtlsdmldlfma");
+  });
+});
+
+// var num;
+// num = 7;
+
+// document.write(num);
+
+// const name = "변용현"
+// document.write("제이름은" + name + "입니다.");
+
+
+// 프로그래밍 언어 : 지나치게 유연 = 리스크가 크다
+// 엄격할수록 리스크가 적어진다.
+// 웹 앱 작업 : 혼자서 독립작업 x ||여러사람이 협업!!
+
+// var num = 7;
+
+// var num = 10;
+
+// console.log(num);
+
+// 재선언&재할당
+
+// const str = "hello world"
+// const str = "hello today"
+
+// const : 재선언 재할당이 원천적으로 불가능한 변수를 선언하고자 할때
+// let : 원칙적으로 재선언은 불가하지만, 재할당은 가능!!
+
+// let str01 = "life is good"
+// str01 = "Today is perfact"
+
+// const data = 5;
+// const str = " hello"
+// const boolean = true;
+
+const result = Number(prompt("당신의 나이를 입력하세요"));
+const tenYear = result + 10;
+
