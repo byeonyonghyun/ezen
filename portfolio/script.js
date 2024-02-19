fb67e9b7b3e341b39494a9fec350ff08570db561
byeonyonghyun~ <qusdydgus0113@gmail.com>
2024-02-19 09:04:06 +0900

    240219

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,24 +1,45 @@
-$(".intro_btn a").click(function(){
-    if($(".intro_btn i").hasClass("active")){
-        $(".intro_btn i").removeClass("active");
-    } else{
-        $(".intro_btn i").addClass("active");
-    }
+$(".intro_btn a").click(function () {
+  if ($(".intro_btn i").hasClass("active")) {
+    $(".intro_btn i").removeClass("active");
+  } else {
+    $(".intro_btn i").addClass("active");
+  }
 });
-$(".intro_btn a").click(function() {
-    setTimeout(function() {
-      $.scrollTo(this.hash || 0, 6200);
-    }, 10000);
-  });
 
-$(".plus").click(function(){
-    if($(".nav_btn a").hasClass("active")){
-        $(".nav_btn a").removeClass("active");
-    } else{
-        $(".nav_btn a").addClass("active");
-    }
+$(".plus").click(function () {
+  if ($(".fa-plus").hasClass("active")) {
+    $(".fa-plus").removeClass("active");
+  } else {
+    $(".fa-plus").addClass("active");
+  }
+});
+
+$(".intro_btn a").click(function () {
+  setTimeout(function () {
+    $.scrollTo(this.hash || 6000, 1000);
+  }, 10000);
+});
+
+$(".plus").click(function () {
+  if ($(".nav_btn a").hasClass("active")) {
+    $(".nav_btn a").removeClass("active");
+  } else {
+    $(".nav_btn a").addClass("active");
+  }
 });
 $(".nav_btn a").click(function () {
-    $.scrollTo(this.hash || 0, 900);
+  $.scrollTo(this.hash || 0, 900);
 });
 
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 1500) {
+    $(".nav_btn").addClass("active");
+  } else {
+    $(".nav_btn").removeClass("active");
+  }
+})
+
+function refreshAndScrollToTop() {
+  location.reload();
+  window.scrollTo(0, 0);
+}
